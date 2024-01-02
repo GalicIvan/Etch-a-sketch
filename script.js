cc9e419c4b3d32a9b49aff6aea8c50fe43ecc37d
GalicIvan <ivangalic2004@gmail.com>
2024-01-02 17:27:06 +0100

    Adding option to chose size of grid (not finished)

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const gridItem = document.createElement('div');
 const gridContainer = document.querySelector('.grid-container');
+const gridContainer2 = document.querySelector('.grid-container2');
+const button = document.querySelector('#button')
+
 
   // Create a 16x16 grid
 for (let i = 0; i < 16 * 16; i++) {
     const gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
-    
+
     gridItem.addEventListener('mouseover', () => {
       gridItem.classList.add('hovered') 
     })
@@ -13,4 +16,27 @@ for (let i = 0; i < 16 * 16; i++) {
     gridContainer.appendChild(gridItem);
 }
 
+let num
+
+function clickedButton() {
+
+  // TODO: overrideat gornji grid i vidit zasto tek iz treceg radi
+
+  button.addEventListener('click', () => {
+    num = prompt('Enter a number: ')
+  })
+  let num2 = parseInt(num, 10)
+  
+  for (let i = 0; i < num2 * num2; i++) {
+    const gridItem = document.createElement('div');
+    gridItem.style.cssText = `width: calc(100% / ${num2} )`;
+    gridItem.classList.add('grid-item')
+    
+    gridItem.addEventListener('mouseover', () => {
+      gridItem.classList.add('hovered') 
+    })
+    
+    gridContainer2.appendChild(gridItem);
+  }
+}
 
